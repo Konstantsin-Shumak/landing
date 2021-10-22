7f452cc9c856512b43f7691791c145d3a7da4b75
Шумак Константин <konstantsin.shumak@gmail.com>
2021-10-22 09:05:02 +0300

    hiden-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-"use strict";
-
-window.onload = function () {
-    //ПИСАТЬ ВЕСЬ КОД В ЭТОЙ ФУНКЦИИ?
+/*==================== MENU SHOW Y HIDDER ====================*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+/*===== MENU SHOW =====*/
+/*Validate if constant exists*/
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
 }
+
+/*===== MENU HIDDEN =====*/
+/*Validate if constant exists*/
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    })
+}
+
+/*==================== MENU SHOW Y HIDDER ====================*/
+const navLinks = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu")
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu")
+}
+navLinks.forEach(link => link.addEventListener("click", linkAction))
