d5c7c6ae6a911ae0a153af68623a9f634d74e674
Шумак Константин <konstantsin.shumak@gmail.com>
2021-10-31 14:14:23 +0300

    add testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,7 +88,7 @@ modalCloses.forEach((modalClose) => {
     });
 });
 /*==================== PORTFOLIO SWIPER ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
 
@@ -103,6 +103,22 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursos: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            sliderPerView: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTION ACTIVE LINK ====================*/
 
